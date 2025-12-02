a8d4bc42db0ae03c12cfb71953e1d3cd84f15ad0
ritsps23 <ritapscosta@ua.pt>
2025-12-02 18:14:10 +0000

    Update vite.config.js

--- Miniprojeto2_React/vite.config.js
+++ Miniprojeto2_React/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  // ------------------------------------------------------------------
+  // Add the base option for correct asset paths on GitHub Pages
+  base: '/TDWminiprojeto2/', 
+  // ------------------------------------------------------------------
+})
